2886803a04c703fdb6953f72db31632fa5e19db2
Unknown <chernyy.dm@yandex.ru>
2018-04-09 21:22:02 +0300

    123

---
+++ Gamez/App.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Gamez
+{
+    
+    public partial class App : Application
+    {
+        public App()
+        {
+            AppDomain currentDomain = AppDomain.CurrentDomain;
+            currentDomain.UnhandledException += new UnhandledExceptionEventHandler(handler);
+        }
+
+        static void handler(object sender, UnhandledExceptionEventArgs args)
+        {
+            Exception e = (Exception)args.ExceptionObject;
+            MessageBox.Show(e.Message);
+        }
+    }
+}
